6a5e0fcf7ef423a7e250a7a624cb1ee1a65a7556
Administrator <fqwyj663775@sina.com>
2019-05-15 16:41:55 +0800

    修改启动类

--- src/main/java/com/yuanwang/WebFrameApplication.java
+++ src/main/java/com/yuanwang/WebFrameApplication.java
@@ -2,12 +2,19 @@ package com.yuanwang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
 @ServletComponentScan
-public class WebFrameApplication {
+@SpringBootApplication
+public class WebFrameApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(WebFrameApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WebFrameApplication.class, args);
 	}

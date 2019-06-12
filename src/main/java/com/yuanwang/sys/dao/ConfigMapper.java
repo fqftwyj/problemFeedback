4bbd58292eeff305473d2d0733d586dcbe466efd
Administrator <fqwyj663775@sina.com>
2019-06-12 14:04:54 +0800

    提交配置信息相关

---
+++ src/main/java/com/yuanwang/sys/dao/ConfigMapper.java
@@ -0,0 +1,21 @@
+package com.yuanwang.sys.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.yuanwang.sys.entity.Config;
+import com.yuanwang.common.core.BaseMapper;
+
+
+/**
+ * ConfigMapper
+ * 系统配置表
+ * @author  crj
+ * @version v1.0.0
+ * 描述：自动生成的代码
+ * 时间：2019-06-12 10:55:12
+ */
+@Repository
+public class ConfigMapper extends BaseMapper<Config, Integer> {
+	
+	
+}
